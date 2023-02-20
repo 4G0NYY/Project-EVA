b3b03c1ed6633a678c22af844f60805aa594894a
Ramon <75240186+LordHeron@users.noreply.github.com>
2023-02-20 11:02:39 +0100

    Update EVA-Core-Test.py
    finally fixed the bitchy python spacing (I think?)

--- EVA-Core-Test.py
+++ EVA-Core-Test.py
@@ -259,17 +259,17 @@ class Predict:
 
     def gan_predict(self):
     	tf.reset_default_graph()
-        gan = GAN(num_features=5, num_historical_days=self.num_historical_days,
-                        generator_input_size=200, is_train=False)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            saver = tf.train.Saver()
-            saver.restore(sess, self.gan_model)
-            clf = joblib.load(self.xgb_model)
-            for sym, date, data in self.data:
-	            features = sess.run(gan.features, feed_dict={gan.X:[data]})
-	            features = xgb.DMatrix(features)
-	            print('{} {} {}'.format(str(date).split(' ')[0], sym, clf.predict(features)[0][1] > 0.5))       #<-- wtf why is python bitching about the spaces and tabs here
+    gan = GAN(num_features=5, num_historical_days=self.num_historical_days,
+        generator_input_size=200, is_train=False)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(sess, self.gan_model)
+        clf = joblib.load(self.xgb_model)
+        for sym, date, data in self.data:
+	        features = sess.run(gan.features, feed_dict={gan.X:[data]})
+	        features = xgb.DMatrix(features)
+	print('{} {} {}'.format(str(date).split(' ')[0], sym, clf.predict(features)[0][1] > 0.5))       #<-- wtf why is python bitching about the spaces and tabs here
 	            
 
 if __name__ == '__main__':
