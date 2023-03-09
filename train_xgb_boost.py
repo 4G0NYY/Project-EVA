b27da4b939abad673c8fb204f3e4e81aa78d3ef5
Ramon <75240186+LordHeron@users.noreply.github.com>
2023-03-09 14:35:26 +0100

    Update train_xgb_boost.py

--- train_xgb_boost.py
+++ train_xgb_boost.py
@@ -29,7 +29,6 @@ class TrainXGBBoost:
             files = [os.path.join('./stock_data', f) for f in os.listdir('./stock_data')]
             for file in files:
                 print(file)
-                #Read in file -- note that parse_dates will be need later
                 df = pd.read_csv(file, index_col='Date', parse_dates=True)
                 df = df[['Open','High','Low','Close','Volume']]
                 labels = df.Close.pct_change(days).map(lambda x: int(x > pct_change/100.0))
